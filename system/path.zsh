372b0f68d79609f2efe8496f0206af3f30c8fd81
Kevin Iverson <kgiverson@yahoo.com>
2012-01-09 19:38:26 -0600

    Updated path to include node npm modules and also added coffee script syntax files for vim.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH=".:bin:/Users/kevin/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$DOTFILES/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export NODE_PATH="/usr/local/lib/node_modules"
