98b281c3fc224d753dc09b52fb0cce72e14f2ccd
Kevin Iverson <kgiverson@yahoo.com>
2013-03-07 17:14:31 -0600

    Accounted for npm binaries in path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH=".:bin:/Users/kevin/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$DOTFILES/bin:$PATH"
+export PATH=".:bin:/Users/kevin/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$DOTFILES/bin:/usr/local/share/npm/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
